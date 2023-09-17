03102485c40408ca05ff1ef22dcd8fd9fc509382
misdarmanto <misdarmanto1922@gmail.com>
2023-09-17 07:19:52 +0700

    create device controller

--- apps/controllers/device/create.ts
+++ apps/controllers/device/create.ts
@@ -1,26 +1,27 @@
 import { type Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { ResponseData } from '../../utilities/response'
+import { type DeviceAttributes } from '../../models/device'
+import { requestChecker } from '../../utilities/requestCheker'
+import { v4 as uuidv4 } from 'uuid'
 
 export const createDevice = async (req: any, res: Response): Promise<any> => {
-  const requestBody = req.body
+  const requestBody = req.body as DeviceAttributes
 
-  console.log(requestBody)
+  const emptyField = requestChecker({
+    requireList: ['deviceName', 'deviceType', 'deviceCategory', 'deviceValue'],
+    requestData: requestBody
+  })
 
-  // const emptyField = requestChecker({
-  //   requireList: ['crudExampleName'],
-  //   requestData: requestBody
-  // })
-
-  // if (emptyField.length > 0) {
-  //   const message = `invalid request parameter! require (${emptyField})`
-  //   const response = ResponseData.error(message)
-  //   return res.status(StatusCodes.BAD_REQUEST).json(response)
-  // }
+  if (emptyField.length > 0) {
+    const message = `invalid request parameter! require (${emptyField})`
+    const response = ResponseData.error(message)
+    return res.status(StatusCodes.BAD_REQUEST).json(response)
+  }
 
   try {
-    // requestBody.crudExampleId = uuidv4()
-    // await CrudExampleModel.create(requestBody)
+    requestBody.crudExampleId = uuidv4()
+    await CrudExampleModel.create(requestBody)
 
     const response = ResponseData.default
     const result = { message: 'success' }
--- apps/controllers/device/index.ts
+++ apps/controllers/device/index.ts
@@ -1,10 +1,7 @@
 import { createDevice } from './create'
-import { findAllCrudExample, findOneCrudExample } from './find'
-import { removeCrudExample } from './remove'
-import { updateCrudExample } from './update'
 
 export const deviceController = {
-  create: createDevice,
+  create: createDevice
   // findAll: findAllCrudExample,
   // findOne: findOneCrudExample,
   // remove: removeCrudExample,
