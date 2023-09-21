4887798098a2f336a8eab901225741cc11d12e4d
unknown <misdarmanto1922@gmail.com>
2023-09-21 17:42:39 +0700

    _

--- apps/controllers/device/index.ts
+++ apps/controllers/device/index.ts
@@ -1,14 +1,14 @@
 import { createDevice } from './create'
 import { findAllAllDevice, findOneDevice } from './find'
 import { removeDevice } from './remove'
-import { statisticDevice } from './statistic'
+// import { statisticDevice } from './statistic'
 import { updateDevice } from './update'
 
 export const deviceController = {
   create: createDevice,
   findAll: findAllAllDevice,
   findOne: findOneDevice,
-  statistic: statisticDevice,
+  // statistic: statisticDevice,
   remove: removeDevice,
   update: updateDevice
 }
