75b4a98e13af8dcb1e206efc4eb310a0b65fefbe
unknown <misdarmanto1922@gmail.com>
2023-10-08 19:10:46 +0700

    fix migration

--- apps/resources/seeders/device.js
+++ apps/resources/seeders/device.js
@@ -11,34 +11,34 @@ module.exports = {
         device_building_id: 'erwer-f6ca-43f8-b8af-ewert4353454',
         device_room_id: 'sdsad-43f8-bsds-sd543534543',
         device_floor: 1,
-        device_token: 'ca93da53-427e-46bb-82fb-217e5b9a3cc1',
+        device_token: 'sdsad-46bb-82fb-werwr4345345345',
         device_status: 'standby'
       },
       {
-        device_id: 'e80d5bfb-4710-43ed-ba9a-74af2b368dd0',
+        device_id: 'e80d5bfb-4710-sdses-rwer4w',
         device_name: 'monitoring suhu',
         device_building_id: 'erwer-f6ca-43f8-b8af-ewert4353454',
         device_room_id: 'sdsad-43f8-bsds-sd543534543',
         device_floor: 5,
-        device_token: 'ca93da53-427e-46bb-82fb-217e5b9a3cc1',
+        device_token: 'ewrwer455-dfgdfgdf0-tyty57676',
         device_status: 'active'
       },
       {
-        device_id: 'e80d5bfb-4710-43ed-ba9a-74af2b368dd0',
+        device_id: 'sdsds-676767-yuyiuyiuyi',
         device_name: 'monitoring AC',
         device_building_id: 'erwer-f6ca-43f8-b8af-ewert4353454',
         device_room_id: 'sdsad-43f8-bsds-sd543534543',
         device_floor: 4,
-        device_token: 'ca93da53-427e-46bb-82fb-217e5b9a3cc1',
+        device_token: 'erwerwer1kjl-rtyry-drfwertwer',
         device_status: 'inactive'
       },
       {
-        device_id: 'e80d5bfb-4710-43ed-ba9a-74af2b368dd0',
+        device_id: 'uiuyiuyi88b-4710-43ed-uiuyiuy',
         device_name: 'monitoring SS',
         device_building_id: 'erwer-f6ca-43f8-b8af-ewert4353454',
         device_room_id: 'sdsad-43f8-bsds-sd543534543',
         device_floor: 5,
-        device_token: 'ca93da53-427e-46bb-82fb-217e5b9a3cc1',
+        device_token: 'sdsad-tyu567-fgsdfgdfgdfg-ertert',
         device_status: 'inactive'
       }
     ])
